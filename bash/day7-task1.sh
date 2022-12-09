30a6a8850033d96de1b25dc18e6779656a411ee6
Stanislav Baiduzhyi <stas.baiduzhyi.dev@gmail.com>
2022-12-09 12:56:14 +0000

    Solution for day 7, no procedural things!

--- bash/day7-task1.sh
+++ bash/day7-task1.sh
@@ -1,71 +1,55 @@
 #!/usr/bin/env bash
 
-set -x
+readonly INPUT_FILE='day7-input.txt'
 
-readonly INPUT_FILE='day7-input-sample.txt'
-
-function sum_file_size_per_dir() {
-  size=0
-  prev_rest='cd ..'
-  while read -r first rest
-  do
-    if [[ $first == '$' ]]
-    then
-#      if [[ $size -ne 0 ]]
-#      then
-#        echo "$size"
-#      fi
-      if [[ "$prev_rest" != 'cd ..' ]]
-      then
-        echo "$size"
-      fi
-      echo "$ $rest"
-      size=0
-    else
-      size=$(( $size + $first ))
-    fi
-    prev_rest="${rest}"
-  done
+function input_into_bash_command() {
+  cat - \
+  | grep -v '$ ls' \
+  | grep -v 'dir ' \
+  | tail -n +2 \
+  | sed \
+  -e 's/^\$ cd \.\.$/cd ../g' \
+  -e 's/^\$ cd \(.*\)/mkdir "\1"\ncd "\1"/g' \
+  -e 's/^\([0-9]*\) \(.*\)$/touch "\1_\2"/g'
 }
 
 function create_physical_structure() {
   tmpdir="$( mktemp -d )"
+  command="$( cat - )"
   (
     cd "$tmpdir"
-    size=0
-    while read -r first second third
-    do
-      if [[ $first == '$' ]]
-      then
-        if [[ $size -ne 0 ]]
-        then
-          touch "$size"
-        fi
-        size=0
-        if [[ "$third" == '..' ]]
-        then
-          cd ..
-        else
-          mkdir "$third"
-          cd "$third"
-        fi
-      else
-        size=$(( $size + $first ))
-      fi
-    done
+    bash -c "$command"
   )
   echo "$tmpdir"
 }
 
-function next() {
+function folder_size() {
+  find "$1" -type f \
+    | sed -e 's;^.*/;;g' -e 's/_.*$//g' \
+    | tr '\n' '+' \
+    | sed 's/+$/\n/' \
+    | bc
+}
+export -f folder_size
+
+function all_folder_sizes() {
   tmpdir="$( cat - )"
-  find "$tmpdir" -type f
+  find "$tmpdir" -type d -exec bash -c 'folder_size "{}"' ';'
 }
 
-cat "$INPUT_FILE" | grep -v '$ ls' | grep -v 'dir ' | tail -n +2 \
+function numbers_below_limit() {
+  grep -E '100000|^[0-9]{1,5}$'
+}
+
+function sum_of_lines() {
+  tr '\n' '+' \
+  | sed 's/+$/\n/' \
+  | bc
+}
+
+cat "$INPUT_FILE" \
+  | input_into_bash_command \
   | create_physical_structure \
-  | next
-#  | sum_file_size_per_dir \
-#  | flatten_dir_tree \
-#  | move_rejected_files
-#  | filter_out_subtrees_with_large_files
+  | all_folder_sizes \
+  | numbers_below_limit \
+  | sum_of_lines
